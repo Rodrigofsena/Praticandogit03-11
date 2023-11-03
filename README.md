7ab420852a33b66d88b2c5a7b2dd3ec1a29e502a
RodrigoSenaGit <senapkd10@gmail.com>
2023-11-03 17:19:59 -0300

    add new title

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Praticandogit03-11
 so uma forma de praticar
+#### Praticando edição
+
+
+nome= rodrigo
+
+print("oi tudo bem{}". format(nome))
---
+++ programa.py
@@ -0,0 +1,11 @@
+print("Olá, meu nome é Luna, eu sou uma IA")
+bemmau = input("Tudo bem? (yes/no) ")
+nome = input("Qual é o seu nome? ")
+bem = "yes"
+mau = "no"
+
+if bemmau == bem:
+    print("Que ótima notícia {}, é sempre bom saber que as coisas estão indo bem para alguem!".format(nome))
+else:
+    print("As coisas vão melhorar, pode acreditar, {}, nem tudo oque eu falo se escreve mas, eu entendo oque é problema.!".format(nome))
+##praticando
